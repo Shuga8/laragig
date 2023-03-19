a5474026bbe4807416fd500b310263e21c3b0e72
Shuga8 <lotocharles8@gmail.com>
2023-03-19 12:20:54 +0100

    working with components

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,5 +1,6 @@
-<div {{$attributes->merge(['class' => 'bg-gray-50 border border-gray-200 rounded p-6'])}}>
+<div {{$attributes->merge(['class' => 'bg-dark border border-gray-200 rounded p-6'])}}>
 
     {{$slot}}
 
 </div> 
+
