567b7364414379ffeefcd41d9bc3545e7fa82038
Shuga8 <lotocharles8@gmail.com>
2023-03-23 14:57:07 +0100

    working with files
    uploading files to db

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -45,6 +45,12 @@ class ListingController extends Controller
             'description' => 'required'
         ]);
 
+        if($request->hasFile('logo')) {
+
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+
+        }
+
         Listing::create($formFields);
 
         return redirect('/')->with('message', 'Listing created successfully!');
