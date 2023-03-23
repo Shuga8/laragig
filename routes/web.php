6bc495a8fe61285bd838c51c2b32ec863b31bcd3
Shuga8 <lotocharles8@gmail.com>
2023-03-23 15:01:54 +0100

    editing gigs functionality

--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,8 @@ Route::get('/listings/create', [ListingController::class, 'create']);
 //store listing data
 Route::post('/listings', [ListingController::class, 'store']);
 
+//Show Edit Form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
 
 //Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
